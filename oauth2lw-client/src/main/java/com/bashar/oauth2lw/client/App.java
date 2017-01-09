ed3d0c3c0692109f1a3caf8914e698fdf988e707
bashar labadi <basharlabadi@gmail.com>
2017-01-08 21:10:41 -0500

    get back the simple client

--- oauth2lw-client/src/main/java/com/bashar/oauth2lw/client/App.java
+++ oauth2lw-client/src/main/java/com/bashar/oauth2lw/client/App.java
@@ -5,23 +5,28 @@ import com.bashar.oauth2lw.core.ResourceServer;
 import com.bashar.oauth2lw.core.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 
+/**
+ * Created by Bashar on 2017-01-08.
+ */
+
 @SpringBootApplication
 @Import({ResourceServer.class, SecurityConfig.class})
-public class Oauth2lwClientApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2lwClientApplication.class, args);
-	}
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+    }
 }
 
 @Configuration
 class AuthorizationServer2 extends AuthorizationServer {
-	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("trusted1").secret("trusted1").scopes("resources")
-				.authorizedGrantTypes("authorization_code").redirectUris("http://localhost:9090/redirect");
-	}
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("trusted1").secret("trusted1").scopes("resources")
+                .authorizedGrantTypes("authorization_code").redirectUris("http://localhost:9090/redirect");
+    }
 }
